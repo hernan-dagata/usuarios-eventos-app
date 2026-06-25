9d435bcfeefd3efc889f034616b8fbf4641fcba1
Hernan Garcia <hernandagata@gmail.com>
2026-06-25 13:36:17 -0500

    se actualiza origen para peticiones del front

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ def options_handler(path: str):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500", "https://hernan-dagata.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
